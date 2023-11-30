0717c924dfd10dc810b5d3a16b62522b3046fc89
^_^ <32826270+hcm444@users.noreply.github.com>
2023-11-30 00:14:25 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import json
+import paho.mqtt.client as mqtt
+app = Flask(__name__)
+mqtt_broker_address = "broker.hivemq.com"
+mqtt_topic = "solar1"
+iot_devices = {}
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(mqtt_topic)
+def on_message(client, userdata, msg):
+    payload = json.loads(msg.payload.decode("utf-8"))
+    # Extracting the sensor ID from the payload
+    sensor_id = payload.get("sensor_id", "unknown")
+    # Update or add the data for the specific sensor ID
+    iot_devices[sensor_id] = payload
+    print(f"Received message from {sensor_id}: {payload}")
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+mqtt_client.connect(mqtt_broker_address, 1883, 60)
+mqtt_client.loop_start()
+@app.route("/")
+def index():
+    # Convert the dictionary values to a list for rendering
+    devices_list = list(iot_devices.values())
+    return render_template("index.html", iot_devices=devices_list)
+if __name__ == "__main__":
+    app.run(debug=True)
